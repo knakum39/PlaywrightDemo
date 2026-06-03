327a56c0a507b28eb7b1860fbbf04a2fcff7e9b2
knakum39 <krishna.nakum39@gmail.com>
2026-06-04 00:40:49 +0530

    Playwright Built-in Locators

--- playwright.config.ts
+++ playwright.config.ts
@@ -30,15 +30,18 @@ export default defineConfig({
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
+    
   },
 
   /* Configure projects for major browsers */
   projects: [
+    
     {
       name: 'chromium',
       use: { ...devices['Desktop Chrome'] },
     },
 
+/*
     {
       name: 'firefox',
       use: { ...devices['Desktop Firefox'] },
@@ -47,7 +50,7 @@ export default defineConfig({
     {
       name: 'webkit',
       use: { ...devices['Desktop Safari'] },
-    },
+    },*/
 
     /* Test against mobile viewports. */
     // {
