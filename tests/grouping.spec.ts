2f090fc628b9630fa8d35d8d0bfa44c540bc638f
Krishna Nakum <136365976+knakum39@users.noreply.github.com>
2026-07-10 17:38:17 +0530

    github workflow test

--- tests/grouping.spec.ts
+++ tests/grouping.spec.ts
@@ -1,2 +1,19 @@
 import {test, expect} from '@playwright/test';
 
+test('Test1', async () => {
+    console.log("this is Test1...")
+});
+
+test('Test2', async () => {
+    console.log("this is Test2...")
+});
+
+test('Test3', async () => {
+    console.log("this is Test3...")
+});
+
+test('Test4', async () => {
+    console.log("this is Test4...")
+});
+
+
